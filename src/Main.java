d1aad3f08365203be7efe678fee57ca9d0c59f89
Senior Java Develope <alina.puris@cnas.gov.md>
2023-10-20 15:01:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Square square= new Square();
+        square.render();
+        Circle circle= new Circle();
+        circle.render();
+        Polygon polygon1= new Square();
+        polygon1.render();
+        Polygon polygon2= new Circle();
+        polygon2.render();
+
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+    }
+}
